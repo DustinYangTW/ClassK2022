b1e752ab34d8928c99e58f5fa36bc2468bc36032
Dustin Kschool <yangkschool@gmail.com>
2022-06-14 15:55:14 +0800

    資料表變數介紹，生命週期就是用完就丟掉

---
+++ SQL/SQL Server 指令碼1/SQL Server 指令碼1/25T-SQL.sql
@@ -0,0 +1,28 @@
+print 'Hello World'
+
+
+--變數
+declare @myName varchar(50) = 'Jack'
+declare @number int
+set @number = 100
+print @myName
+print @NUMBER
+
+declare @salary money = 50000
+print '你的薪水為' + cast(@salary as varchar)
+
+declare @birthday datetime = '2022/06-14'
+print convert(varchar , @birthday,112)
+
+declare @birthday datetime
+select @birthday = 生日 from 學生 where 學號 = 'S002'
+print convert(varchar , @birthday,112)
+
+
+
+
+declare @StuBirthday table (
+	name nvarchar(30),
+	birthday datetime
+)
+insert into @StuBirthday values('任我行','2022-6/14')
