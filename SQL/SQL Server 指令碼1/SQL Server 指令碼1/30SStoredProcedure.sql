09f762fafb158505f7c82c71151fde0793d8488c
Dustin Kschool <yangkschool@gmail.com>
2022-06-20 15:33:52 +0800

    介紹預存程序，可以傳值的方法

--- SQL/SQL Server 指令碼1/SQL Server 指令碼1/30SStoredProcedure.sql
+++ SQL/SQL Server 指令碼1/SQL Server 指令碼1/30SStoredProcedure.sql
@@ -11,3 +11,49 @@ end
 ----------------------------------------
 --執行
 exec getOrderDetails
+
+create proc deawStar
+as
+	begin
+		declare @i int =1 ,@sum nvarchar(6)='' 
+		while @i<5
+		begin
+		 set @sum += '*'
+		 print @sum
+		 set @i+=1
+		end
+end
+go
+exec deawStar
+go
+
+create proc deawStar2
+	@c int
+as
+begin
+		declare @i int =1 ,@sum nvarchar(max)='' 
+		while @i<=@c
+		begin
+		 set @sum += '*'
+		 print @sum
+		 set @i+=1
+		end
+end
+
+exec deawStar2 10
+exec deawStar2 @c = 10
+
+create proc emplyeeDataRow
+	@i int ,@j int
+as 
+begin
+	select * 
+	from 員工
+	order by 身份證字號
+	offset 3 rows
+	fetch next 2 rows only
+end
+
+----exce
+exec emplyeeDataRow 2,3
+exec emplyeeDataRow @j=2,@i=3 --可以不按照順序
