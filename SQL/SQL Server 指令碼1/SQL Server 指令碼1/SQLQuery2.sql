4e59fcde4fe83ff00f3435711600e5f4b5f439c0
Dustin Kschool <yangkschool@gmail.com>
2022-06-20 14:25:50 +0800

    介紹預存程序

---
+++ SQL/SQL Server 指令碼1/SQL Server 指令碼1/SQLQuery2.sql
@@ -0,0 +1,6 @@
+SELECT          教授.教授編號, 班級.課程編號, 員工.薪水
+FROM              教授 INNER JOIN
+                        員工 ON 教授.身份證字號 = 員工.身份證字號 INNER JOIN
+                        班級 ON 教授.教授編號 = 班級.教授編號 INNER JOIN
+                        學生 ON 班級.學號 = 學生.學號 CROSS JOIN
+                        客戶業績
