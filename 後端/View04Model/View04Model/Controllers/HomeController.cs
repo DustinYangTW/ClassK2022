c95fd2d1d70d1b0bfb3b951571f5161a90ad7fc4
Dustin Kschool <yangkschool@gmail.com>
2022-06-20 10:05:53 +0800

    建立ViewModels，並且讓他有資料型別+View話

---
+++ 後端/View04Model/View04Model/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using View04Model.Models;
+using View04Model.ViewModels;
+
+namespace View04Model.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbEmployeeEntities db = new dbEmployeeEntities()
+;        // GET: Home
+        public ActionResult Index(int depId = 1)
+        {
+            VMEmpoyee vmep = new VMEmpoyee()
+            {
+                department = db.tDepartment.ToList(),
+                employee = db.tEmployee.Where(m => m.fDepId == depId).ToList()
+            };
+
+            //var employee = db.tEmployee.ToList();
+            return View(vmep);
+        }
+    }
+}
---
+++ 後端/View04Model/View04Model/ViewModels/VMEmpoyee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using View04Model.Models;
+
+namespace View04Model.ViewModels
+{
+    public class VMEmpoyee
+    {
+        public List<tDepartment> department { get; set; }
+        public List<tEmployee> employee { get; set; }
+    }
+}
