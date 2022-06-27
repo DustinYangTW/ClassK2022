8304a6868b9b11a8e12d2769e461bb762f47292d
Dustin Kschool <yangkschool@gmail.com>
2022-06-27 09:16:10 +0800

    設定身份證驗證器，需要繼承相關方法，並且複寫bool方法

---
+++ 後端/05CustomCheck/05CustomCheck/Models/Member.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace _05CustomCheck.Models
+{
+    public class Member
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required(ErrorMessage ="姓名為必填欄位")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "身份證字號為必填欄位")]
+        [RegularExpression("[A-Z[12][0-9]{8}", ErrorMessage = "身份證字號資料錯誤")]
+        [ChechIDName]
+        public string IDNumber { get; set; }//身份證字號
+        public class ChechIDName : ValidationAttribute
+        {
+            public override bool IsValid(object value)
+            {
+                int sum = 0;
+                string id = value.ToString();
+
+
+
+                sum = 130;
+                if (sum % 10 == 0)
+                    return true;
+
+                return false;
+            }
+
+        }
+    }
+}
