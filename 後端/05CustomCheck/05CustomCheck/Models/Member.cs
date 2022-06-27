ca906f0872b479f5cc079544cd1bd7b1a999c945
Dustin Kschool <yangkschool@gmail.com>
2022-06-27 10:32:03 +0800

    設定身份證驗證器的規則

--- 後端/05CustomCheck/05CustomCheck/Models/Member.cs
+++ 後端/05CustomCheck/05CustomCheck/Models/Member.cs
@@ -12,7 +12,7 @@ namespace _05CustomCheck.Models
         [Key]
         public int id { get; set; }
 
-        [Required(ErrorMessage ="姓名為必填欄位")]
+        [Required(ErrorMessage = "姓名為必填欄位")]
         [StringLength(50)]
         public string Name { get; set; }
 
@@ -26,16 +26,39 @@ namespace _05CustomCheck.Models
             {
                 int sum = 0;
                 string id = value.ToString();
-
-
-
-                sum = 130;
+                const string eng = "ABCDEFGHJKLMNPQRSTUVXYZIO";
+                string t = id.Substring(0, 1);
+                int intEng = eng.IndexOf(t) + 10;
+                int n1 = intEng / 10;
+                int n2 = intEng % 10;
+                int[] a = new int[9];
+                for (int i = 0; i < 9; i++)
+                {
+                    a[i] = Convert.ToInt32(id.Substring(i + 1, 1));
+                }
+                //int j = 0;
+                //for (int i = 0; i < 9; i++)
+                //{
+                //    if (8 - i <= 0)
+                //    {
+                //        j = 1;
+                //    }
+                //    else
+                //    {
+                //        j = 8 - i;
+                //    }
+                //    sum += a[i] * (8 - i);
+                //}
+                for (int i = 0; i < 8; i++)
+                {
+                    sum += a[i] * (8 - i);
+                }
+                sum += a[8] + n1 + n2 * 9;
+                //sum = n1 + n2 * 9 + a[0] * 8 + a[1] * 7 + a[2] * 6 + a[3] * 5 + a[4] * 4 + a[5] * 3 + a[6] * 2 + a[7] + a[8];
                 if (sum % 10 == 0)
                     return true;
-
                 return false;
             }
-
         }
     }
 }
