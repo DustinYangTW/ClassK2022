301fa1d5cedece284163070145048876fb4c8ccf
Dustin Kschool <yangkschool@gmail.com>
2022-07-06 13:48:26 +0800

    存peopleData資料(尚未完成)

---
+++ 後端/HomeBackProject/HomeBackProject/Controllers/PeopleController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HomeBackProject.Models;
+
+namespace HomeBackProject.Controllers
+{
+    public class PeopleController : Controller
+    {
+        ActiondbController actiondbController = new ActiondbController();
+        HomeDataEntities db = new HomeDataEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public ActionResult Create(AccountData accountData)
+        //{
+        //    return actiondbController.Create(db, db.AccountDatas, accountData);
+        //}
+        [HttpPost]
+        public ActionResult Create(PeopleData peopleData)
+        {
+            return actiondbController.Create(db,db.PeopleDatas, peopleData);
+        }
+    }
+}
