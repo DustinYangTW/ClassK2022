b86e419094a4c2caffc83bbad019b238753d9ba7
Dustin Kschool <yangkschool@gmail.com>
2022-07-06 09:18:01 +0800

    處理頁印用css

--- SQL/SQL Server 指令碼1/SQL Server 指令碼1/Function02.sql
+++ SQL/SQL Server 指令碼1/SQL Server 指令碼1/Function02.sql
@@ -1,8 +1,18 @@
-create function fnStudentCourseQuery()
-	returns table
-as
+create function fnOffsetFetchNext(@m int,@n int)
+	returns @result table(
+		sn int identity,
+		id char(10),
+		[name] varchar(12),
+		salary money
+	)
 begin
-	
+	insert into @result
+	select e.身份證字號,e.姓名,e.薪水
+	from 員工 as e
 
+			delete from @result where sn<@m or sn>@n
+	return
 
 end
+
+select * from dbo.fnOffsetFetchNext(4,6)
