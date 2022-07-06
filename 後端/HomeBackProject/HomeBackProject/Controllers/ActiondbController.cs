293343bf71af7b137a324b7f9888165f069411bd
Dustin Kschool <yangkschool@gmail.com>
2022-07-06 11:04:37 +0800

    建立主要控制增刪的控制器

--- 後端/HomeBackProject/HomeBackProject/Controllers/ActiondbController.cs
+++ 後端/HomeBackProject/HomeBackProject/Controllers/ActiondbController.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 using System.Web.Mvc;
+using HomeBackProject.Models;
 
 namespace HomeBackProject.Controllers
 {
     public class ActiondbController : Controller
     {
-        public ActionResult Create<T>()
+        public ActionResult Create<T>(DbContext db, DbSet dbSet, T item)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                dbSet.Add(item);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete<T>(DbContext db, DbSet dbSet, T item)
+        {
+            if (ModelState.IsValid)
+            {
+                dbSet.Remove(item);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
