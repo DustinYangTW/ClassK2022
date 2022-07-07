cfde4312511e9e58e64be49abe1c0779697573bb
Dustin Kschool <yangkschool@gmail.com>
2022-07-07 09:59:54 +0800

    新增API資料，並且套用北風資料庫的資料，轉成Json

---
+++ 後端/WebAPI/WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        NorthwindEntities1 db =new NorthwindEntities1();
+        // GET api/values
+        public IEnumerable<Customers> Get()
+        {
+            var customers = db.Customers.ToList();
+            return customers;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
