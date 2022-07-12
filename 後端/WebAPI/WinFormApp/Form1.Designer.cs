d13cc96a278af344b75e0fec68bc45ed7d0f9b37
Dustin Kschool <yangkschool@gmail.com>
2022-07-12 10:56:01 +0800

    vup /y

---
+++ 後端/WebAPI/WinFormApp/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WinFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Btn_1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(83, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(416, 148);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Btn_1
+            // 
+            this.Btn_1.Location = new System.Drawing.Point(102, 230);
+            this.Btn_1.Name = "Btn_1";
+            this.Btn_1.Size = new System.Drawing.Size(144, 69);
+            this.Btn_1.TabIndex = 1;
+            this.Btn_1.Text = "取得客戶資料";
+            this.Btn_1.UseVisualStyleBackColor = true;
+            this.Btn_1.Click += new System.EventHandler(this.Btn_1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Btn_1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Btn_1;
+    }
+}
+
