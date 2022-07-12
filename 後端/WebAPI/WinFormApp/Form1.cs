f2cb5cbac65fce604caeea9ab640b4d052d067ea
Dustin Kschool <yangkschool@gmail.com>
2022-07-12 11:36:04 +0800

    WinForm串API

--- 後端/WebAPI/WinFormApp/Form1.cs
+++ 後端/WebAPI/WinFormApp/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
 
 namespace WinFormApp
 {
@@ -19,7 +20,39 @@ namespace WinFormApp
 
         private void Btn_1_Click(object sender, EventArgs e)
         {
-            listBox1.Items.Add("大家好");
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:53860/api/Values");
+            HttpResponseMessage resp = client.GetAsync("http://localhost:53860/api/Values").Result;
+            IEnumerable<Rootobject> data = null;
+
+            if (resp.IsSuccessStatusCode)
+            {
+                data = resp.Content.ReadAsAsync<IEnumerable<Rootobject>>().Result;
+
+                foreach (var item in data)
+                {
+                    listBox1.Items.Add("客戶名稱:" + item.CompanyName + ", 聯絡人:" + item.ContactName + ", 職稱:" + item.ContactTitle);
+                }
+
+            }
+
+
+        }
+        public class Rootobject
+        {
+            public string CustomerID { get; set; }
+            public string CompanyName { get; set; }
+            public string ContactName { get; set; }
+            public string ContactTitle { get; set; }
+            public string Address { get; set; }
+            public string City { get; set; }
+            public object Region { get; set; }
+            public string PostalCode { get; set; }
+            public string Country { get; set; }
+            public string Phone { get; set; }
+            public string Fax { get; set; }
+            public object[] Orders { get; set; }
+            public object[] Orders1 { get; set; }
         }
     }
 }
