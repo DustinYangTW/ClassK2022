26cd09b4e1e720f92c6a09025d0474b32bddb152
Dustin Kschool <yangkschool@gmail.com>
2022-07-13 09:32:40 +0800

    設定程式2; yji4

---
+++ SQL/SQL Server 指令碼1/SQL Server 指令碼1/28DDlTrigger.sql
@@ -0,0 +1,14 @@
+create trigger readinly_table on database
+for drop_table,alter_table
+as
+begin
+	rollback
+end
+
+disable trigger readinly_table on database
+
+alter table ½Òµ{
+	disable trigger showNewCiurse
+
+alter table ½Òµ{
+	enable trigger showNewCiurse
---
+++ 後端/WebAPI/WebAPI/Controllers/Covid19Controller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Net.Http;
+using WebAPI.Models;
+using Newtonsoft.Json;
+
+namespace WebAPI.Controllers
+{
+    public class Covid19Controller : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            string url = "https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=4051&limited=TWN";
+
+            HttpClient client = new HttpClient();
+            client.MaxResponseContentBufferSize = Int32.MaxValue;
+            var resp = await client.GetStringAsync(url);
+
+            var collection = JsonConvert.DeserializeObject<IEnumerable<Covid_19>>(resp);//反序列化
+
+            return View(collection);
+        }
+    }
+}
