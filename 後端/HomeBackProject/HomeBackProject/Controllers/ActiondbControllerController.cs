bdbea3719e1693f5289a723c217aa0ce4b43b6ea
Dustin Kschool <yangkschool@gmail.com>
2022-07-13 14:20:12 +0800

    cjo4jb4

---
+++ 後端/HomeBackProject/HomeBackProject/Controllers/ActiondbControllerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using HomeBackProject.Models;
+
+namespace HomeBackProject.Controllers
+{
+    public class ActiondbController : Controller
+    {
+        public ActionResult Create<T>(DbContext db, DbSet dbSet, T item)
+        {
+            if (ModelState.IsValid)
+            {
+                dbSet.Add(item);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete<T>(DbContext db, DbSet dbSet, T item)
+        {
+            if (ModelState.IsValid)
+            {
+                dbSet.Remove(item);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
