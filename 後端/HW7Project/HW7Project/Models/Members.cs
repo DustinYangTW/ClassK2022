ed7f1ba5a93a9ed17c6f4e330f0993f583c367ef
Dustin Kschool <yangkschool@gmail.com>
2022-07-18 09:37:21 +0800

    建立Members

---
+++ 後端/HW7Project/HW7Project/Models/Members.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace HW7Project.Models
+{
+    public class Members
+    {
+        [Key]
+        [DisplayName("會員編號")]
+        public int MemberID { get; set; }
+        [DisplayName("會員帳號")]
+        [Required(ErrorMessage = "請填寫帳號")]
+        [StringLength(20, ErrorMessage = "不能超過20個字")]
+        public string Account { get; set; }
+        [Required(ErrorMessage = "請填寫密碼")]
+        [MinLength(8, ErrorMessage = "密碼最少要8碼")]
+        [MaxLength(20, ErrorMessage = "密碼最多20碼")]
+        [DataType(DataType.Password)]
+        [DisplayName("會員密碼")]
+        public string PassWord { get; set; }
+        [DisplayName("會員名字")]
+        [StringLength(20, ErrorMessage = "最多輸入20個字")]
+        public string MemberName { get; set; }
+        [DisplayName("會員照片檔案路徑")]
+        public string MenberPhotoFile { get; set; }
+        [DisplayName("會員生日")]
+        [Required(ErrorMessage = "請填寫生日")]
+        public DateTime MemberBirdthday { get; set; }
+        [DisplayName("建立時間")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public DateTime CreateDate { get; set; }
+
+
+    }
+}
