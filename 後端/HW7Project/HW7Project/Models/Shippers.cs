f1d60d1d354a6905c80514dc788c693bdc4b8553
Dustin Kschool <yangkschool@gmail.com>
2022-07-18 09:31:33 +0800

    建立Shippers

---
+++ 後端/HW7Project/HW7Project/Models/Shippers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace HW7Project.Models
+{
+    public class Shippers
+    {
+        [Key]
+        [DisplayName("送貨方式編號")]
+        public int ShipID { get; set; }
+        [DisplayName("送貨方式")]
+        [StringLength(20,ErrorMessage ="最多輸入20個字")]
+        public string ShipVia { get; set; }
+    }
+}
