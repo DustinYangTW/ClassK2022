b4ab38e091ce0f749051fb7b882702cb99a48488
Dustin Kschool <yangkschool@gmail.com>
2022-07-27 09:30:02 +0800

    新增登入系統(剛開始檔案)

---
+++ 後端/HomeBackProject/HomeBackProject/Controllers/HomeManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeBackProject.Controllers
+{
+    public class HomeManagerController : Controller
+    {
+        // GET: HomeManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
