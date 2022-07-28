079561d2a2ba6d34d8be6a8abf2423b8fa810b66
Dustin Kschool <yangkschool@gmail.com>
2022-07-28 09:25:34 +0800

    增刪修改查

---
+++ 後端/07ADonet_Send_differentdb/07ADonet_Send_differentdb/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using _07ADonet_Send_differentdb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace _07ADonet_Send_differentdb.Controllers
+{
+    public class HomeController : Controller
+    {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["aaa"].ConnectionString);
+        
+        public ActionResult Index()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from 學生", conn);
+            return View();
+        }
+    }
+}
