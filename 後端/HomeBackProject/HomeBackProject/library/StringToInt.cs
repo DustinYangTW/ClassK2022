f7e8a270c6579cfa127856536395cd165d0b3051
Dustin <schoolKyang@gmail.com>
2022-07-29 19:47:32 +0800

    把自動編碼(ID)給獨立開來

--- 後端/HomeBackProject/HomeBackProject/library/StringToInt.cs
+++ 後端/HomeBackProject/HomeBackProject/library/StringToInt.cs
@@ -15,15 +15,13 @@ namespace HomeBackProject.library
         /// <param name="idIn">db裡面的ID最後一碼</param>
         /// <param name="data">ID第一碼的英文</param>
         /// <returns></returns>
-        public string changIDNumber(string idIn,string data)
+        public string changIDNumber(string idIn, string data)
         {
-            int id = Int32.Parse(idIn.Substring(2))+1;
+            int id = Int32.Parse(idIn.Substring(2)) + 1;
 
             string CheckID = "A" + id.ToString().PadLeft(9, '0');
 
             return CheckID;
         }
-
-
     }
 }
