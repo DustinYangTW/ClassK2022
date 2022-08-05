03138be0252ff2eef012f3170aaf8ece76771e62
Dustin Kschool <yangkschool@gmail.com>
2022-08-05 12:36:42 +0800

    插入MetadataType

--- 後端/HomeBackProject/HomeBackProject/Models/HomeData.cs
+++ 後端/HomeBackProject/HomeBackProject/Models/HomeData.cs
@@ -11,7 +11,10 @@ namespace HomeBackProject.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using MetaDataHomeBackProject.Models;
+    [MetadataType(typeof(MetaHomeData))]
     public partial class HomeData
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
