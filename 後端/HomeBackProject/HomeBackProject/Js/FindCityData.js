9d1defff574ff48a95977ab0d43014270efc73b0
Dustin <schoolKyang@gmail.com>
2022-08-14 17:54:59 +0800

    新建會員的前端驗證

--- 後端/HomeBackProject/HomeBackProject/Js/FindCityData.js
+++ 後端/HomeBackProject/HomeBackProject/Js/FindCityData.js
@@ -1,16 +1,21 @@
 ﻿
 function mainIdex() {
     var selectCountyValue = $('#selectCounty').val();
-    console.log(selectCountyValue);
+    //console.log(selectCountyValue);
     var selecttwValue = $('#selectCity').children().text();
-    console.log(selecttwValue);
+    //console.log(selecttwValue);
+
+    var cityid = "";
+    var cityadd = "";
     $('#selectCity').empty();
     if (selecttwValue == "請先選擇城市") {
-        var cityadd = `<option disabled style="display: none" selected value>請選擇</option>`;
+        cityadd = `<option disabled style="display: none" selected value>請選擇</option>`;
     } else {
-        var cityadd = `<option disabled style="display: none"  value>${selecttwValue}</option>`;
+        cityadd = `<option disabled style="display: none"  value>${selecttwValue}</option>`;
     }
     $('#selectCity').append(cityadd);
+
+    var arr = [];
     $.ajax({
         url: `https://api.nlsc.gov.tw/other/ListTown1/${selectCountyValue}`,
         type: 'GET',
@@ -18,10 +23,18 @@ function mainIdex() {
         success: function (data) {
             //console.log(data)
             for (i = 0; i < $(data).find("townname").length; i++) {
-                var cityid = data.getElementsByTagName("townname")[i].firstChild.nodeValue;
-                console.log(cityid);
-                var cityadd = `<option value="${cityid}">${cityid}</option>`;
-                $('#selectCity').append(cityadd);
+                cityid = data.getElementsByTagName("townname")[i].firstChild.nodeValue;
+                //console.log(cityid);
+
+                arr = Array.from(cityid);
+                //console.log(arr);
+                //console.log(arr.indexOf("縣"));
+                //console.log(arr.indexOf("市"));
+
+                if (arr.indexOf("縣") == -1 && arr.indexOf("市") == -1) {
+                    cityadd = `<option value="${cityid}">${cityid}</option>`;
+                    $('#selectCity').append(cityadd);
+                }
             }
         }
     });
