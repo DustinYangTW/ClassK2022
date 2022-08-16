3d6bb3d51efbc2384f2f1baccbb558f470ad932c
Dustin Kschool <yangkschool@gmail.com>
2022-08-16 11:38:57 +0800

    黑盒子教學

---
+++ 後端/HW7Project/HW7Project/App_Start/FilterConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HW7Project.Controllers;
+
+namespace HW7Project.App_Start
+{
+    public class FilterConfig
+    {
+        public static void ResgisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new LogReporter());
+        }
+    }
+}
