963fb29439268726ce3737934db394ee71495aec
Dustin Kschool <yangkschool@gmail.com>
2022-08-25 11:27:24 +0800

    加入購物車(初版)，有邏輯問題

--- 後端/HW7Project/HW7Project/Controllers/HomeController.cs
+++ 後端/HW7Project/HW7Project/Controllers/HomeController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HW7Project.Models;
 
 namespace HW7Project.Controllers
 {
     public class HomeController : Controller
     {
+        HW7ProjectContext db = new HW7ProjectContext();
         // GET: Home
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult ProcductList()
+        {
+            var products = db.Products.Where(p=>p.Discontinued==false).ToList();
+            return View(products);
         }
     }
 }
