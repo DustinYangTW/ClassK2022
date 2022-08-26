469fdf5e412956fa5734342dc8e5e20f1c1d1360
Dustin <yangkschool@gmail.com>
2022-08-26 23:49:24 +0800

    新增綜合設定，並且完成一個可以新增資料進資料庫的Create

--- 後端/HomeBackProject/HomeBackProject/Controllers/OthersSetController.cs
+++ 後端/HomeBackProject/HomeBackProject/Controllers/OthersSetController.cs
@@ -5,16 +5,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
+using HomeBackProject.library;
 
 namespace HomeBackProject.Controllers
 {
     public class OthersSetController : Controller
     {
         private HomeDataEntities db = new HomeDataEntities();
-        //public ActionResult Index()
-        //{
-        //    return View();
-        //}
+        private ActiondbController actiondbController = new ActiondbController();
 
         public ActionResult Index(string idNmae)
         {
@@ -26,20 +25,104 @@ namespace HomeBackProject.Controllers
                 };
                 ViewBag.CheckData = idNmae;
                 ViewBag.CheckView = "會員身份設定";
+
+                //int pagesize = 10;
+                //var pagedList = vMOthersSet.PeopleRankData.OrderBy(m=>m.PeopleRank).ToPagedList(page, pagesize);
                 return View(vMOthersSet);
             }
-            //VMOthersSet vMOthersSet = new VMOthersSet()
-            //{
-            //    //TerritoryTypeData = db.TerritoryTypeData.ToList(),
-            //    //SaleTypeData = db.SaleTypeData.ToList(),
-            //    //ProgramData = db.ProgramData.ToList(),
-            //    //PeopleRankData = db.PeopleRankData.ToList(),
-            //    //HomeTypeData = db.HomeTypeData.ToList(),
-            //    //CityTypeData = db.CityTypeData.ToList(),
-            //    //CarTypeData = db.CarTypeData.ToList(),
-            //    //ADTypeData = db.ADTypeData.ToList()
-            //};
+
+            else if (idNmae == "Program")
+            {
+                VMOthersSet vMOthersSet = new VMOthersSet()
+                {
+                    ProgramData = db.ProgramData.ToList()
+                };
+                ViewBag.CheckData = idNmae;
+                ViewBag.CheckView = "會員方案設定";
+                return View(vMOthersSet);
+            }
+
+            else if (idNmae == "Home")
+            {
+                VMOthersSet vMOthersSet = new VMOthersSet()
+                {
+                    HomeTypeData = db.HomeTypeData.ToList()
+                };
+                ViewBag.CheckData = idNmae;
+                ViewBag.CheckView = "房屋類型設定";
+                return View(vMOthersSet);
+            }
+
+            else if (idNmae == "Territory")
+            {
+                VMOthersSet vMOthersSet = new VMOthersSet()
+                {
+                    TerritoryTypeData = db.TerritoryTypeData.ToList()
+                };
+                ViewBag.CheckData = idNmae;
+                ViewBag.CheckView = "土地類型設定";
+                return View(vMOthersSet);
+            }
+
+            else if (idNmae == "Car")
+            {
+                VMOthersSet vMOthersSet = new VMOthersSet()
+                {
+                    CarTypeData = db.CarTypeData.ToList()
+                };
+                ViewBag.CheckData = idNmae;
+                ViewBag.CheckView = "車位類型設定"; 
+                return View(vMOthersSet);
+            }
+
+            else if (idNmae == "Sale")
+            {
+                VMOthersSet vMOthersSet = new VMOthersSet()
+                {
+                    SaleTypeData = db.SaleTypeData.ToList()
+                };
+                ViewBag.CheckData = idNmae;
+                ViewBag.CheckView = "銷售類型設定";
+                return View(vMOthersSet);
+            }
+
+            else if (idNmae == "AD")
+            {
+                VMOthersSet vMOthersSet = new VMOthersSet()
+                {
+                    ADTypeData = db.ADTypeData.ToList()
+                };
+                ViewBag.CheckData = idNmae;
+                ViewBag.CheckView = "廣告設定";
+                return View(vMOthersSet);
+            }
+
+            else if (idNmae == "City")
+            {
+                VMOthersSet vMOthersSet = new VMOthersSet()
+                {
+                    CityTypeData = db.CityTypeData.ToList()
+                };
+                ViewBag.CheckData = idNmae;
+                ViewBag.CheckView = "縣市設定";
+                return View(vMOthersSet);
+            }
+
+
             ViewBag.CheckView = "請在左側選擇要操作的系統";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string idNmae,string toDBName)
+        {
+            if (ModelState.IsValid)
+            {
+                PeopleRankData peopleRankData = new PeopleRankData();
+                peopleRankData.PeopleRank = toDBName;
+                return actiondbController.Create(db, db.PeopleRankData, peopleRankData);
+            }
+
             return View();
         }
     }
---
+++ 後端/HomeBackProject/HomeBackProject/Controllers/PeopleRankDatasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HomeBackProject.Models;
+
+namespace HomeBackProject.Controllers
+{
+    public class PeopleRankDatasController : Controller
+    {
+        private HomeDataEntities db = new HomeDataEntities();
+
+        // GET: PeopleRankDatas
+        public ActionResult Index()
+        {
+            return View(db.PeopleRankData.ToList());
+        }
+
+        // GET: PeopleRankDatas/Details/5
+        public ActionResult Details(byte? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PeopleRankData peopleRankData = db.PeopleRankData.Find(id);
+            if (peopleRankData == null)
+            {
+                return HttpNotFound();
+            }
+            return View(peopleRankData);
+        }
+
+        // GET: PeopleRankDatas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PeopleRankDatas/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "HomeTSaleStateID,PeopleRank")] PeopleRankData peopleRankData)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PeopleRankData.Add(peopleRankData);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(peopleRankData);
+        }
+
+        // GET: PeopleRankDatas/Edit/5
+        public ActionResult Edit(byte? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PeopleRankData peopleRankData = db.PeopleRankData.Find(id);
+            if (peopleRankData == null)
+            {
+                return HttpNotFound();
+            }
+            return View(peopleRankData);
+        }
+
+        // POST: PeopleRankDatas/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "HomeTSaleStateID,PeopleRank")] PeopleRankData peopleRankData)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(peopleRankData).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(peopleRankData);
+        }
+
+        // GET: PeopleRankDatas/Delete/5
+        public ActionResult Delete(byte? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PeopleRankData peopleRankData = db.PeopleRankData.Find(id);
+            if (peopleRankData == null)
+            {
+                return HttpNotFound();
+            }
+            return View(peopleRankData);
+        }
+
+        // POST: PeopleRankDatas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(byte id)
+        {
+            PeopleRankData peopleRankData = db.PeopleRankData.Find(id);
+            db.PeopleRankData.Remove(peopleRankData);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
