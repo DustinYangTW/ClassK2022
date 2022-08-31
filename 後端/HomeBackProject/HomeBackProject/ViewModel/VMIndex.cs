c5983e6f32cc5b6bcb4e65d1b0f79867d7443f2b
Dustin Kschool <yangkschool@gmail.com>
2022-08-31 10:08:17 +0800

    準備開始做前端總介面

---
+++ 後端/HomeBackProject/HomeBackProject/ViewModel/VMIndex.cs
@@ -0,0 +1,15 @@
+﻿using HomeBackProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeBackProject.ViewModel
+{
+    public class VMIndex
+    {
+        //土地類型
+        public List<HomeData> HomeDatas{ get; set; }
+        public List<PeopleData> PeopleDatas{ get; set; }
+    }
+}
