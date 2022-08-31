7f8baa932b9fb9ae6b9359103772f6e04940baa4
Dustin <yangkschool@gmail.com>
2022-08-31 19:55:01 +0800

    創建好房屋購買方案(草稿)

--- 後端/HomeBackProject/HomeBackProject/Controllers/PeopleBuySomethingController.cs
+++ 後端/HomeBackProject/HomeBackProject/Controllers/PeopleBuySomethingController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
 
 namespace HomeBackProject.Controllers
 {
@@ -35,5 +36,40 @@ namespace HomeBackProject.Controllers
             ViewBag.Win = "購買成功!! 儲值 " + point + " 點 " + " ， 您目前共有 : " + Math.Round((double)peopleData.PeopleCash,0)+" 點";
             return View("buyPoints");
         }
+
+        [LoginCkeck]
+        public ActionResult buyADForHome()
+        {
+            return View(db.ADTypeData.ToList());
+        }
+
+        [LoginCkeck]
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult buyADForHouse(string id, int point,short ADNumber)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HomeData homeData = db.HomeData.Find(id);
+            ADTypeData aDTypeData = db.ADTypeData.Find(ADNumber);
+            PeopleData peopleData = db.PeopleData.Find(homeData.HomePeopleID);
+
+            if((int)peopleData.PeopleCash < point)
+            {
+                ViewBag.Win = "點數不足，點選旁邊按鈕購買點數!!!";
+                return View("buyPoints");
+            }
+
+            string ADName = aDTypeData.ADName;
+            homeData.HomeADLevel = ADNumber;
+            peopleData.PeopleCash = peopleData.PeopleCash - point;
+            db.Entry(peopleData).State = EntityState.Modified;
+            db.SaveChanges();
+            ViewBag.Win = "購買成功!! 儲值方案為 " + ADName + " ， 您點數剩餘 : " + Math.Round((double)peopleData.PeopleCash, 0) + " 點";
+            return View("buyPoints");
+        }
+
     }
 }
