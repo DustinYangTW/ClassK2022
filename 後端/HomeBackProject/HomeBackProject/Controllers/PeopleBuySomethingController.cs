ba729191e17c31c2871a5c58d3a1e2458f6aeae1
Dustin <yangkschool@gmail.com>
2022-08-31 23:38:16 +0800

    購買方案系統有問題，需要修改

--- 後端/HomeBackProject/HomeBackProject/Controllers/PeopleBuySomethingController.cs
+++ 後端/HomeBackProject/HomeBackProject/Controllers/PeopleBuySomethingController.cs
@@ -1,5 +1,4 @@
 ﻿using HomeBackProject.Models;
-using HomeBackProject.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -38,18 +37,12 @@ namespace HomeBackProject.Controllers
             return View("buyPoints");
         }
 
-
-        [LoginCkeck]
+        //[LoginCkeck]
         public ActionResult buyADForHome(string id)
         {
-            List<HomeData> homeData = new List<HomeData>();
-            homeData.Add(db.HomeData.Find(id));
-            VMHouseAD vMHouseAD = new VMHouseAD()
-            {
-                ADTypeData = db.ADTypeData.ToList(),
-                HomeData = homeData
-            };
-            return View(vMHouseAD);
+            var homeData = db.HomeData.Find(id);
+            ViewBag.HomeID = homeData.HomeID;
+            return View(db.ADTypeData.ToList());
         }
 
         [LoginCkeck]
@@ -62,22 +55,24 @@ namespace HomeBackProject.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             HomeData homeData = db.HomeData.Find(id);
-            ADTypeData aDTypeData = db.ADTypeData.Find(ADNumber);
             PeopleData peopleData = db.PeopleData.Find(homeData.HomePeopleID);
 
-            if((int)peopleData.PeopleCash < point)
+            int cash = Session["userRank"].ToString() == "4"? 500000 : (int)peopleData.PeopleCash;
+
+            if (cash < point)
             {
                 ViewBag.Win = "點數不足，點選旁邊按鈕購買點數!!!";
                 return View("buyPoints");
             }
 
-            string ADName = aDTypeData.ADName;
-            homeData.HomeADLevel = ADNumber;
-            peopleData.PeopleCash = peopleData.PeopleCash - point;
+            
+            homeData.HomeADLevel = (short)ADNumber;
+            peopleData.PeopleCash = Session["userRank"].ToString() == "4" ? peopleData.PeopleCash : peopleData.PeopleCash - (decimal)point;
             db.Entry(peopleData).State = EntityState.Modified;
+            db.Entry(homeData).State = EntityState.Modified;
             db.SaveChanges();
-            ViewBag.Win = "購買成功!! 儲值方案為 " + ADName + " ， 您點數剩餘 : " + Math.Round((double)peopleData.PeopleCash, 0) + " 點";
-            return View("buyPoints");
+            //ViewBag.Win = "購買成功!! 儲值方案為 " + ADName + " ， 您點數剩餘 : " + Math.Round((double)peopleData.PeopleCash, 0) + " 點";
+            return RedirectToAction("Index", "HomeDatas");
         }
 
     }
