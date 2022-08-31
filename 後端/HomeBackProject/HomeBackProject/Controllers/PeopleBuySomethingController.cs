3d2613f2d2178d67475c4dfc66ddb13e8185f0a0
Dustin Kschool <yangkschool@gmail.com>
2022-08-31 16:23:57 +0800

    製作出儲值系統

---
+++ 後端/HomeBackProject/HomeBackProject/Controllers/PeopleBuySomethingController.cs
@@ -0,0 +1,39 @@
+﻿using HomeBackProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeBackProject.Controllers
+{
+    public class PeopleBuySomethingController : Controller
+    {
+        private HomeDataEntities db = new HomeDataEntities();
+        // GET: PeopleBuySomething
+        [LoginCkeck]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [LoginCkeck]
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult HomeSaleTypeDelete(string id,int point)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PeopleData peopleData = db.PeopleData.Find(id);
+            peopleData.PeopleCash = peopleData.PeopleCash + point;
+            db.Entry(peopleData).State = EntityState.Modified;
+            db.SaveChanges();
+            ViewBag.Win = "購買成功!! 儲值" + point + " 點 " + " 您目前共有 : " + Math.Round((double)peopleData.PeopleCash,0)+" 點";
+            return View("index");
+        }
+    }
+}
